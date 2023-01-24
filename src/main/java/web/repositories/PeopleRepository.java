69ee301b23aeaf7931c9b33101dee48d63b7d687
DmitryMigal <119596182+DmitryMigal@users.noreply.github.com>
2023-01-24 11:06:38 +0300

    12

---
+++ src/main/java/web/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package web.repositories;
+
+import model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+}
